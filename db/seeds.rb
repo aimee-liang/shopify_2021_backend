c1e503b59e5d7dec5ecf0514abe8b083a7f94ae9
aimee-liang <aimeeeliang@gmail.com>
2020-12-25 17:48:23 -0500

    Included gitignore file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'uri'
+require 'net/http'
+require 'JSON'
+
+uri = URI.parse("http://www.omdbapi.com/?apikey=[yourkey]&")
